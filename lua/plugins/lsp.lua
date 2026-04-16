8504f81ae1e66cb6fee13279dc6c2c58f07d625e
Luke Lewis <luke.lewis@retool.com>
2026-04-16 15:09:28 -0700

    use tsgo

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -168,7 +168,7 @@ return {
 		--  - settings (table): Override the default settings passed when initializing the server.
 		--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 		local servers = {
-			-- TypeScript is handled by typescript-tools.nvim (see typescript-tools.lua)
+			tsgo = {},
 			bashls = {},
 			-- NOTE: Using eslint_d via nvim-lint instead (see lint.lua)
 			-- eslint = {
@@ -220,7 +220,6 @@ return {
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
 			"stylua", -- Used to format Lua code
-			"eslint_d", -- Used by nvim-lint for JS/TS linting
 		})
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -1,17 +1,2 @@
-return {
-	"pmizio/typescript-tools.nvim",
-	dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-	ft = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-	opts = {
-		settings = {
-			tsserver_max_memory = 16384,
-			separate_diagnostic_server = true,
-			publish_diagnostic_on = "insert_leave",
-			tsserver_file_preferences = {
-				includeInlayParameterNameHints = "all",
-				includeCompletionsForModuleExports = true,
-				quotePreference = "auto",
-			},
-		},
-	},
-}
+-- Previously used typescript-tools.nvim; now using tsgo via lspconfig (see lsp.lua)
+return {}
